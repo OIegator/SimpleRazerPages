2cee19bc44c1be773142be58411141499b913bbf
OIegator <64355448+OIegator@users.noreply.github.com>
2023-03-05 13:59:31 +0300

    Add header and footer to RazerPage

---
+++ SimpleRazerPages/Pages/Pricingcshtml.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleRazerPages.Pages
+{
+    public class PricingcshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
